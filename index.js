c5233ce31c212e08307ff7747c2579f522a000b1
devShaurya <devshaurya1001@gmail.com>
2021-10-16 21:37:36 +0530

    send message in response

--- index.js
+++ index.js
@@ -16,15 +16,19 @@ exports.handler = async (event, context, callback) => {
     if (err !== null) {
         statusCode = 500;
         body.error = err;
+        body.message = "failed";
     } else {
         body.requestToken = requestToken;
+
         const { accessToken, err: err1 } = await getAccessToken(requestToken);
         if (err1 != null) {
             statusCode = 500;
             body.error = err1;
+            body.message = "failed";
         } else {
             statusCode = 200;
             body.accessToken = accessToken;
+            body.message = "success";
         }
     }
 
