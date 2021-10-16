f68858c826e68db202065bb9e1cf903e90fbd733
devShaurya <devshaurya1001@gmail.com>
2021-10-16 21:08:10 +0530

    changed response to include access token

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 "use-strict";
+const { getAccessToken } = require("./getAccessToken");
 const { getRequestToken } = require("./getRequestToken");
 
-exports.handler = async (event, context, callback) => {
+const handler = async (event, context, callback) => {
     var body = {
         error: null,
         requestToken: null,
         message: null,
+        accessToken: null,
     };
     var statusCode;
 
     const { requestToken, err } = await getRequestToken();
-    body.requestToken = requestToken;
-    body.error = err;
 
-    if (body.error !== null) {
+    if (err !== null) {
         statusCode = 500;
+        body.error = err;
     } else {
-        statusCode = 200;
+        body.requestToken = requestToken;
+        const { accessToken, err: err1 } = await getAccessToken(requestToken);
+        if (err1 != null) {
+            statusCode = 500;
+            body.error = err1;
+        } else {
+            statusCode = 200;
+            body.accessToken = accessToken;
+        }
     }
 
     const response = {
@@ -29,3 +38,5 @@ exports.handler = async (event, context, callback) => {
     };
     return response;
 };
+
+handler().then((response) => console.log(response));
