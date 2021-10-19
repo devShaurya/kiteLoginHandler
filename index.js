2b6272a9d940acf4a73540a393f346a1ea0d09c3
devShaurya <devshaurya1001@gmail.com>
2021-10-19 21:11:08 +0530

    changed to just send request token

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use-strict";
-const { getAccessToken } = require("./getAccessToken");
+const fetch = require("node-fetch");
 const { getRequestToken } = require("./getRequestToken");
 
 exports.handler = async (event, context, callback) => {
@@ -7,38 +7,30 @@ exports.handler = async (event, context, callback) => {
         error: null,
         requestToken: null,
         message: null,
-        accessToken: null,
     };
-    var statusCode;
 
     const { requestToken, err } = await getRequestToken();
 
     if (err !== null) {
-        statusCode = 500;
-        body.error = err;
+        body.error = err.toString;
         body.message = "failed";
     } else {
         body.requestToken = requestToken;
-
-        const { accessToken, err: err1 } = await getAccessToken(requestToken);
-        if (err1 != null) {
-            statusCode = 500;
-            body.error = err1;
-            body.message = "failed";
-        } else {
-            statusCode = 200;
-            body.accessToken = accessToken;
-            body.message = "success";
-        }
+        body.message = "success";
     }
 
-    const response = {
-        statusCode: statusCode,
+    const requestOptions = {
+        method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*",
         },
-        body: body,
+        body: JSON.stringify(body),
     };
+    const response = await fetch(
+        "https://21b5-117-242-98-136.ngrok.io/broker/kiteLoginHandlerWebhook",
+        requestOptions
+    ).then((response) => response.json());
+
     return response;
 };
