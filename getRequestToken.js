9e8901cdf05fbc3fd6c043795b9c5e47143cc9e9
devShaurya <devshaurya1001@gmail.com>
2021-10-21 14:23:05 +0530

    added encryption for env variables

--- getRequestToken.js
+++ getRequestToken.js
@@ -1,13 +1,62 @@
 "use-strict";
 
 const chromium = require("chrome-aws-lambda");
-const kiteApiKey = process.env.KITE_API_KEY;
-const kiteUserId = process.env.KITE_USER_ID;
-const kiteUserPassword = process.env.KITE_USER_PASSWORD;
-const kiteUserPin = process.env.KITE_USER_PIN;
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-2" });
+
+const functionName = process.env.AWS_LAMBDA_FUNCTION_NAME;
+const kiteApiKeyEncrypted = process.env.KITE_API_KEY;
+const kiteUserIdEncrypted = process.env.KITE_USER_ID;
+const kiteUserPasswordEncrypted = process.env.KITE_USER_PASSWORD;
+const kiteUserPinEncrypted = process.env.KITE_USER_PIN;
 const kiteApiVersion = process.env.KITE_API_VERSION;
+var kiteApiKey, kiteUserId, kiteUserPassword, kiteUserPin;
+
+const decryptEnvData = async () => {
+    if (!kiteApiKey || !kiteUserId || !kiteUserPassword || !kiteUserPin) {
+        // Decrypt code should run once and variables stored outside of the
+        // function handler so that these are decrypted once per container
+        const kms = new AWS.KMS();
+        try {
+            const reqKiteApiKey = {
+                CiphertextBlob: Buffer.from(kiteApiKeyEncrypted, "base64"),
+                EncryptionContext: { LambdaFunctionName: functionName },
+            };
+            var data = await kms.decrypt(reqKiteApiKey).promise();
+            kiteApiKey = data.Plaintext.toString("ascii");
+
+            const reqKiteUserId = {
+                CiphertextBlob: Buffer.from(kiteUserIdEncrypted, "base64"),
+                EncryptionContext: { LambdaFunctionName: functionName },
+            };
+            data = await kms.decrypt(reqKiteUserId).promise();
+            kiteUserId = data.Plaintext.toString("ascii");
+
+            const reqKiteUserPassword = {
+                CiphertextBlob: Buffer.from(
+                    kiteUserPasswordEncrypted,
+                    "base64"
+                ),
+                EncryptionContext: { LambdaFunctionName: functionName },
+            };
+            data = await kms.decrypt(reqKiteUserPassword).promise();
+            kiteUserPassword = data.Plaintext.toString("ascii");
+
+            const reqKiteUserPin = {
+                CiphertextBlob: Buffer.from(kiteUserPinEncrypted, "base64"),
+                EncryptionContext: { LambdaFunctionName: functionName },
+            };
+            data = await kms.decrypt(reqKiteUserPin).promise();
+            kiteUserPin = data.Plaintext.toString("ascii");
+        } catch (err) {
+            console.log("Decrypt error:", err);
+            throw err;
+        }
+    }
+};
 
 exports.getRequestToken = async () => {
+    await decryptEnvData();
     var browser,
         requestToken,
         err = null;
